c636b713cd423a35c38c722ae362cab81788b243
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-07-25 18:15:23 +0530

    shared colors created

---
+++ Android/app/src/main/java/com/example/salsa/util/SharedColors.kt
@@ -0,0 +1,19 @@
+package com.example.salsa.util
+
+import androidx.annotation.ColorInt
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+enum class SharedColors(
+    @ColorInt private val darkColor: Long,
+    @ColorInt private val lightColor: Long
+) {
+    SURFACE(darkColor = 0xFF121212, lightColor = 0xFFEDEDED),
+    ON_SURFACE(darkColor = 0xFFFCFCFC, lightColor = 0xFF030303),
+    SURFACE_CONTAINER(darkColor = 0xFF2A2A2A, lightColor = 0xffD5D5D5),
+    ON_SURFACE_CONTAINER(darkColor = 0xFF727272, lightColor = 0xFF8D8D8D);
+
+    val color: Color @Composable get() = Color(color = if (isSystemInDarkTheme()) this.darkColor else this.lightColor)
+
+}
