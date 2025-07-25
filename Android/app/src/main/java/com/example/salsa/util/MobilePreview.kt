6bc48c7f1e3849927d52cab44e38d3303c4d696e
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-07-26 01:25:56 +0530

    new colors and annotations added

--- Android/app/src/main/java/com/example/salsa/util/MobilePreview.kt
+++ Android/app/src/main/java/com/example/salsa/util/MobilePreview.kt
@@ -1,10 +1,24 @@
 package com.example.salsa.util
 
+import android.content.res.Configuration
 import androidx.compose.ui.tooling.preview.Preview
 
 @Preview(
     widthDp = 360,
     heightDp = 800,
-    showBackground = true
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO
 )
 annotation class MobilePreview
+
+@Preview(
+    widthDp = 360,
+    heightDp = 800,
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+annotation class MobilePreviewDark
+
+@MobilePreview
+@MobilePreviewDark
+annotation class DualThemePreview
--- Android/app/src/main/java/com/example/salsa/util/SharedColors.kt
+++ Android/app/src/main/java/com/example/salsa/util/SharedColors.kt
@@ -12,7 +12,8 @@ enum class SharedColors(
     SURFACE(darkColor = 0xFF121212, lightColor = 0xFFEDEDED),
     ON_SURFACE(darkColor = 0xFFFCFCFC, lightColor = 0xFF030303),
     SURFACE_CONTAINER(darkColor = 0xFF2A2A2A, lightColor = 0xffD5D5D5),
-    ON_SURFACE_CONTAINER(darkColor = 0xFF727272, lightColor = 0xFF8D8D8D);
+    ON_SURFACE_CONTAINER(darkColor = 0xFF727272, lightColor = 0xFF8D8D8D),
+    CURSOR(darkColor = 0xFFFFFFFF, lightColor = 0xFF000000);
 
     val color: Color @Composable get() = Color(color = if (isSystemInDarkTheme()) this.darkColor else this.lightColor)
 
